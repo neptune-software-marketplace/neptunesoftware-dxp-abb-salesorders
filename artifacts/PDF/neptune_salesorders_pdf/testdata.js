29b435d6e70389bc5a5e53c899060ca7193e92e9
Admin <cezar.strugariu@neptune-software.com>
2026-06-12 15:01:26 +0100

    Added missing PDF Template

---
+++ artifacts/PDF/neptune_salesorders_pdf/testdata.js
@@ -0,0 +1,41 @@
+{
+    "data": [{
+        "ORDERNUMBER": "100",
+        "ITEMNAME": "Gear",
+        "ITEMQUANTITY": "5",
+        "MATERIAL": "Metal",
+        "DESCRIPTION": "Rock Solid",
+        "DOCTYPE": "5678",
+        "ITEMNUBER": "123",
+        "DIVISION": "North",
+        "SALESUNIT": "1001",
+        "CURRENCY": "EUR",
+        "STATUS": "Pending"
+    },
+    {
+        "ORDERNUMBER": "200",
+        "ITEMNAME": "Rod",
+        "ITEMQUANTITY": "1",
+        "MATERIAL": "Metal",
+        "DESCRIPTION": "Rock Solid",
+        "DOCTYPE": "5679",
+        "ITEMNUBER": "124",
+        "DIVISION": "North",
+        "SALESUNIT": "1001",
+        "CURRENCY": "EUR",
+        "STATUS": "Pending"
+    },
+    {
+        "ORDERNUMBER": "100",
+        "ITEMNAME": "Gear",
+        "ITEMQUANTITY": "5",
+        "MATERIAL": "Metal",
+        "DESCRIPTION": "Rock Solid",
+        "DOCTYPE": "5678",
+        "ITEMNUBER": "123",
+        "DIVISION": "North",
+        "SALESUNIT": "1001",
+        "CURRENCY": "EUR",
+        "STATUS": "Pending"
+    }]
+}
